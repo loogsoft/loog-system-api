bd857a505afb52a7ba8328429e65196db9512a5f
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-06-25 14:50:29 -0300

    merge:(merge) merge

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -19,7 +19,8 @@ export class EmailService {
     try {
       await this.resend.emails.send({
         from: 'Loog System <onboarding@resend.dev>',
-        to: 'loogsoftware@gmail.com',
+        // to: 'loogsoftware@gmail.com',
+        to: 'andersonmendesdesouza2007@gmail.com',
         subject: 'Código de verificação – Loog System',
         html: `
   <div style="
