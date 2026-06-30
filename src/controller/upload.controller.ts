053f00ce3d100de7aebe6ddb1af0613235df6d23
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-06-30 10:53:44 -0300

    merge:(merge) merge

--- src/controller/upload.controller.ts
+++ src/controller/upload.controller.ts
@@ -2,16 +2,13 @@ import {
   Controller,
   Post,
   UploadedFile,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 
 import { FileInterceptor } from '@nestjs/platform-express';
-
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UploadService } from 'src/services/upload.service';
 
-@UseGuards(JwtAuthGuard)
+
 @Controller('upload')
 export class UploadController {
   constructor(private uploadService: UploadService) {}
