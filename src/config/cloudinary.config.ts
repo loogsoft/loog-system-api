aa6e08311e6b9dfaff0f2f3b3b570f45472e452b
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-09-25 16:47:13 -0300

    merge

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -4,14 +4,20 @@ const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
 const apiKey = process.env.CLOUDINARY_API_KEY;
 const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
-if (!cloudName) {
-  throw new Error('Variável cloudName não encotrada!');
-}
-if (!apiKey) {
-  throw new Error('Variável apiKey não encotrada!');
-}
-if (!apiSecret) {
-  throw new Error('Variável apiSecret não encotrada!');
+if (process.env.NODE_ENV === 'production') {
+  console.log('Validando Cloudinary...');
+
+  if (!cloudName) {
+    throw new Error('Variável CLOUDINARY_CLOUD_NAME não encontrada!');
+  }
+
+  if (!apiKey) {
+    throw new Error('Variável CLOUDINARY_API_KEY não encontrada!');
+  }
+
+  if (!apiSecret) {
+    throw new Error('Variável CLOUDINARY_API_SECRET não encontrada!');
+  }
 }
 
 cloudinary.config({
